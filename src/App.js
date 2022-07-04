63f3828f3f437bb65876bde379e4c217c3a83817
arsal uddin <arsaluddin134@gmail.com>
2022-07-04 18:16:34 +0530

    lets generate positive quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { render } from "@testing-library/react";
+
+class App extends React.Component {
+
+    state = { advice: ''};
+
+    componentDidMount() {
+        this.fetchadvice();
+    }
+
+    fetchadvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+        .then((responce) => {
+            const {advice} = responce.data.slip;
+            
+            this.setState({advice});
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        const {advice} = this.state;
+        return(
+            <div className="app">
+                 <div className="card">
+                     <h1 className="heading">{advice}</h1>
+                     <button className="btn" onClick={this.fetchadvice}>
+                        <span>New Quote</span>
+                     </button>
+                 </div>
+            </div>
+        );
+    }
+
 }
 
 export default App;
