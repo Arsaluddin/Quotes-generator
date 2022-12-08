19de843e2220bb10e384ef236d4fc85e990ede1a
arsal uddin <arsaluddin134@gmail.com>
2022-12-08 18:48:37 +0530

    functional components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import React from "react";
+import React, {  useEffect, useState} from "react";
 import axios from "axios";
 
 import './App.css';
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 
-class App extends React.Component {
+const App = () => {
 
-    state = { advice: ''};
+    const [state,setState] = useState();
 
-    componentDidMount() {
-        this.fetchadvice();
-    }
+   // useEffect is used because when first time page reloads we want to see the quotes
+    useEffect( () => {
+        fetchadvice();
+      },[]);
 
-    fetchadvice = () => {
-        axios.get('https://api.adviceslip.com/advice')
-        .then((responce) => {
-            const {advice} = responce.data.slip;
-            
-            this.setState({advice});
-        })
-        .catch((error) => {
+      async function fetchadvice  ()  {
+       
+         try {
+            const result = await axios.get("https://api.adviceslip.com/advice")
+             const data = result.data.slip.advice;
+            setState(data);
+         }
+         catch(error) {
             console.log(error);
-        });
+         }
+
     }
 
-    render() {
-        const {advice} = this.state;
+
+
+   
+    
+        const advice = state;
         return(
             <div className="app">
                  <div className="card">
                      <h1 className="heading">{advice}</h1>
-                     <button className="btn" onClick={this.fetchadvice}>
+                     <button className="btn" onClick={fetchadvice}>
                         <span>New Quote</span>
                      </button>
                  </div>
             </div>
         );
-    }
+
 
 }
 
